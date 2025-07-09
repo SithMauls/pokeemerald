8625fa7780632969f39242fa75d44e15e7650075
SithMauls <34665250+SithMauls@users.noreply.github.com>
2025-07-09 02:11:11 +0100

    Cleaner loot drop implementation

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -3330,7 +3330,7 @@ static void Cmd_getexp(void)
               | BATTLE_TYPE_BATTLE_TOWER
               | BATTLE_TYPE_EREADER_TRAINER)))
         {
-            gBattleScripting.getexpState = 6; // goto last case
+            gBattleScripting.getexpState = 8; // goto last case
         }
         else
         {
@@ -3572,7 +3572,7 @@ static void Cmd_getexp(void)
         }
         break;
     case 6: // check if wild Pokémon has a hold item after fainting
-        if (!(gBattleTypeFlags & BATTLE_TYPE_TRAINER) && GetBattlerSide(gBattlerFainted) == B_SIDE_OPPONENT && holdItem != ITEM_NONE)
+        if (!(gBattleTypeFlags & BATTLE_TYPE_TRAINER) && holdItem != ITEM_NONE)
         {
             PrepareStringBattle(STRINGID_PKMNDROPPEDITEM, gBattleStruct->expGetterBattlerId);
             gBattleScripting.getexpState++; // add item to bag
